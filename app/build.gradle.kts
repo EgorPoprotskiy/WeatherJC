bb2142640d81c3c9d3588bfb94e711b417243c17
EgorPoprotskiy <egorpoprotskiy@gmail.com>
2025-08-26 23:40:49 +1000

    fix: add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,11 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     //Kotlinx Serialization
     alias(libs.plugins.kotlin.serialization)
-//    kotlin("plugin.serialization") version "2.1.20"
+//    kotlin("plugin.serialization") version "2.1.20" - старый синтаксис
+    //Room
+    alias(libs.plugins.ksp)
 }
+
 
 android {
     namespace = "com.egorpoprotskiy.weatherjc"
@@ -43,15 +46,20 @@ android {
 }
 
 dependencies {
-
+    // AndroidX & Kotlin
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    //ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    // Тесты
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -60,9 +68,32 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     //Kotlinx Serialization
-    implementation(libs.kotlinx.serialization.json)
-    //Retrofit
+    // Навигация
+    implementation(libs.androidx.navigation.compose)
+    //Для свайпа
+    implementation(libs.androidx.compose.material)
+    // Корутины
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Retrofit и Serialisation
     implementation(libs.retrofit)
     implementation(libs.retrofit.serialization)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(platform(libs.okhttp.bom)) // Важно для BOM
 
+    // Room (база данных)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // Coil (загрузка изображений)
+    implementation(libs.coil.compose)
+
+    implementation(libs.androidx.compose.runtime.livedata)
+    // Или если ты добавляешь по одной:
+//    implementation(libs.retrofit.core)
+    // Если ты используешь бандл:
+    implementation(libs.bundles.network.deps)
 }
