57160ac163255a112f17646d472d72fea545a9fe
EgorPoprotskiy <egorpoprotskiy@gmail.com>
2025-08-30 17:13:26 +1000

    feat: Add Hilt module for dependency injection2

---
+++ app/src/main/java/com/egorpoprotskiy/weatherjc/WeatherApplication.kt
@@ -0,0 +1,11 @@
+package com.egorpoprotskiy.weatherjc
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+//8
+
+@HiltAndroidApp
+class WeatherApplication: Application() {
+
+}
