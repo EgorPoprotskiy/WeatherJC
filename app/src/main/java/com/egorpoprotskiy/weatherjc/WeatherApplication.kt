67bd299dea8fb4317357dc228ca68662d2a599d1
EgorPoprotskiy <egorpoprotskiy@gmail.com>
2025-08-30 17:38:15 +1000

    feat: Add WeatherViewModel

--- app/src/main/java/com/egorpoprotskiy/weatherjc/WeatherApplication.kt
+++ app/src/main/java/com/egorpoprotskiy/weatherjc/WeatherApplication.kt
@@ -3,7 +3,7 @@ package com.egorpoprotskiy.weatherjc
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-//8
+//8 + добавить "android:name=".WeatherApplication" в манифест
 
 @HiltAndroidApp
 class WeatherApplication: Application() {
---
+++ app/src/main/java/com/egorpoprotskiy/weatherjc/presentation/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,20 @@
+package com.egorpoprotskiy.weatherjc.presentation.viewmodel
+
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import com.egorpoprotskiy.weatherjc.data.repository.WeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+//9
+
+@HiltViewModel
+class WeatherViewModel @Inject constructor(
+    private val weatherRepository: WeatherRepository
+): ViewModel() {
+
+    suspend fun getWeather() {
+
+    }
+}
