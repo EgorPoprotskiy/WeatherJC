234356e2bf6cf7c8483de5ca48e2e01695f59b07
EgorPoprotskiy <egorpoprotskiy@gmail.com>
2025-09-06 12:38:14 +1000

    feat: edit_AppContainer

--- app/src/main/java/com/egorpoprotskiy/weatherjc/AppContainer.kt
+++ app/src/main/java/com/egorpoprotskiy/weatherjc/AppContainer.kt
@@ -1,24 +1,12 @@
 package com.egorpoprotskiy.weatherjc
 
-import com.egorpoprotskiy.weatherjc.api.WeatherApi
+import com.egorpoprotskiy.weatherjc.api.RetrofitClient
 import com.egorpoprotskiy.weatherjc.data.repository.WeatherRepository
 import com.egorpoprotskiy.weatherjc.data.repository.WeatherRepositoryImpl
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
 
 //10  Это будет "мозг" нашего приложения, который будет создавать и хранить все зависимости.
 class AppContainer {
-    private val baseUrl = "https://api.openweathermap.org"
-    private val retrofit = Retrofit.Builder()
-        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(baseUrl)
-        .build()
-    private val weatherApi: WeatherApi by lazy {
-        retrofit.create(WeatherApi::class.java)
-    }
     val weatherRepository: WeatherRepository by lazy {
-        WeatherRepositoryImpl(weatherApi = weatherApi)
+        WeatherRepositoryImpl(weatherApi = RetrofitClient.weatherApi)
     }
 }
